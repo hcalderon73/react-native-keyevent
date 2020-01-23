473be88ccb384b5549edea0f3475835f470ff631
hcalderon73 <calderonhumberto@gmail.com>
2020-01-23 17:26:23 +0100

    Update KeyEventModule.java

--- android/src/main/java/com/github/kevinejohn/keyevent/KeyEventModule.java
+++ android/src/main/java/com/github/kevinejohn/keyevent/KeyEventModule.java
@@ -69,8 +69,10 @@ public class KeyEventModule extends ReactContextBaseJavaModule {
 
     private WritableMap getJsEventParams(int keyCode, KeyEvent keyEvent, Integer repeatCount) {
         WritableMap params = new WritableNativeMap();
+        
         int action = keyEvent.getAction();
         char pressedKey = (char) keyEvent.getUnicodeChar();
+        boolean modifiers = keyEvent.isModifierKey(keyCode);
 
         if (keyEvent.getAction() == KeyEvent.ACTION_MULTIPLE && keyCode == KeyEvent.KEYCODE_UNKNOWN) {
             String chars = keyEvent.getCharacters();
@@ -79,12 +81,22 @@ public class KeyEventModule extends ReactContextBaseJavaModule {
             }
         }
 
-        if (repeatCount != null) {
-            params.putInt("repeatcount", repeatCount);
+        if(keyCode == KeyEvent.KEYCODE_TAB) {
+            params.putBoolean("isTabsPressed", true);
         }
 
+        params.putString("displayLabel", String.valueOf(keyEvent.getDisplayLabel()));
+        params.putInt("scanCode", keyEvent.getScanCode());
+        params.putInt("unicodeChar", keyEvent.getUnicodeChar());
+        params.putInt("getModifiers", keyEvent.getModifiers());
+        params.putString("Number", String.valueOf(keyEvent.getNumber()));
+        params.putBoolean("isShiftPressed", keyEvent.isShiftPressed());
+        params.putBoolean("isLongPress", keyEvent.isLongPress());
+        params.putBoolean("isCapsLockOn", keyEvent.isCapsLockOn());
+        params.putBoolean("isCtrlPressed", keyEvent.isCtrlPressed());
         params.putInt("keyCode", keyCode);
         params.putInt("action", action);
+        params.putBoolean("isModifierKey", modifiers);
         params.putString("pressedKey", String.valueOf(pressedKey));
 
         return params;
